0c1c68b9d7f6105ad26f3eb4aeab4238584ab22e
dazephster <austinlee12378@gmail.com>
2025-04-04 13:39:32 -0500

    updated base

--- personal-site/vite.config.js
+++ personal-site/vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/<REPO>/',
+  base: '/<psychic-octo-waddle>/',
   plugins: [
     vue(),
     vueDevTools(),
